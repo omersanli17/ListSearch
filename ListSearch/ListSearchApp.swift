196c287965e231cae07d0b5b04d9d879be70c9fe
omer <sanli.omer@metu.edu.tr>
2022-01-31 20:04:40 +0300

    Initial Commit

---
+++ ListSearch/ListSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListSearchApp.swift
+//  ListSearch
+//
+//  Created by omer sanli on 31.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ListSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
